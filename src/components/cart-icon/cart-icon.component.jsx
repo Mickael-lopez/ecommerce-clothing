1ba96fe9a2ef7cee0c032823bbb89d82ec1b4f3b
Virtuel Sushi plugin <Mickael.lopez@virtueldesign.be>
2020-04-21 11:16:50 +0200

    updating ou app with reselect, replaced cart selectors with memorized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart-action.js';
+import {selectCartItemsCount} from '../../redux/cart/cart-selectors.js';
 
 import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.style.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart_icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping_icon'/>
-        <span className='item_count'>0</span>
+        <span className='item_count'>{itemCount}</span>
     </div>
 );
 
@@ -18,4 +19,8 @@ const mapDispatchToProps = dispatch => ({
    toggleCartHidden: () => dispatch(toggleCartHidden()) 
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+   itemCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
